043ea54fb8de672f7442d54e5eef9abcb069ce07
Rachel Wyatt <rw126@evansville.edu>
2019-04-24 11:32:49 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -12,6 +12,6 @@ function getRepositories() {
 function displayRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
   console.log(repos)
-  <ul>${repos.map(repo => '<li>' + repo.url + ' - <a href="#" data-repo="' + repo.html_url + '" onclick="getCommits(this)">Get Commits</a>' +  '- <a href="#" data-branch="' + repo.branches_url + '" onclick="getBranches(this)">Get Branches</a> </li>').join('')}</ul>
+  const repoList = `<ul>${repos.map(repo => '<li>' + repo.url + ' - <a href="#" data-repo="' + repo.html_url + '" onclick="getCommits(this)">Get Commits</a>' +  '- <a href="#" data-branch="' + repo.branches_url + '" onclick="getBranches(this)">Get Branches</a> </li>').join('')}</ul>`
 	document.getElementById("repositories").innerHTML = repoList
 }
