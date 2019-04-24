9815d6e73b0101556b819fa00f8d8f0a359afc64
Rachel Wyatt <rw126@evansville.edu>
2019-04-24 11:29:02 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,3 +7,10 @@ function getRepositories() {
   req.setRequestHeader("Authorization", `token`)
   req.send();
 }
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '<br> - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a><br>-<a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
