2b1f2c6681b4a187a50f01d986e779b6cdeb0a5a
Rachel Wyatt <rw126@evansville.edu>
2019-04-24 11:38:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -15,3 +15,12 @@ function displayRepositories(event, data) {
   const repoList = `<ul>${repos.map(repo => '<li>' + repo.url + ' - <a href="#" data-repo="' + repo.html_url + '" onclick="getCommits(this)">Get Commits</a>' +  '- <a href="#" data-branch="' + repo.branches_url + '" onclick="getBranches(this)">Get Branches</a> </li>').join('')}</ul>`
 	document.getElementById("repositories").innerHTML = repoList
 }
+
+function getCommits(el) {
+  const name = el.dataset.repository; 
+  const req = new XMLHttpRequest();
+  let user = ducment.getElementById("username").value;
+  req.addEventListener("load", displayCommits); 
+  req.open("GET", `https://api.github.com/repos/${user}/` + name + '/commits');
+  req.send();
+}
