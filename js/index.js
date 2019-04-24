6ec0f9b5693c8222cc1ec0a27cdad1fe84a5a4a0
Rachel Wyatt <rw126@evansville.edu>
2019-04-24 11:46:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -30,3 +30,12 @@ function displayCommits(event, data) {
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.commit.author.name + '</strong> - ' + commit.author.login + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
+
+function getBranches(el) {
+  const name = el.dataset.repository; 
+  const req = new XMLHttpRequest();
+  let user = document.getElementById("username").value;
+  req.addEventListener("load", displayBranches);
+  req.open("GET", `https://api.github.com/repos/${user}/` + name + '/branches');
+  req.send();
+}
