4d717045694f7e598db0206a1a950cbf8d29ae97
Rachel Wyatt <rw126@evansville.edu>
2019-04-24 11:17:40 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 function getRepositories() {
+  event.preventDefault() 
+  const currentUser = document.getElementById(`username`).value
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.open('GET', (`https://api.github.com/users/` + currentUser + `/repos`));
+  req.setRequestHeader("Authorization", `token`)
   req.send();
 }
